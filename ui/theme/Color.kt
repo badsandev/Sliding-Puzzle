ae9028690cfc61f3f4faaf155f76a04a25c9502d
Alejocm11 <alejocatanomotta@gmail.com>
2026-03-11 12:20:16 -0500

    solucion de ultimos errores de compatibilidad

---
+++ ui/theme/Color.kt
@@ -0,0 +1,22 @@
+package com.taller.puzle.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val ColorHueco     = Color(0xFF1A1A3A)
+val ColorResuelto  = Color(0xFF69F0AE)
+val ColorCanMove   = Color(0xFFCE93D8)
+val ColorInPlace   = Color(0xFF42A5F5)
+val ColorNormal    = Color(0xFF3949AB)
+val ColorFondo     = Color(0xFF1A1A2E)
+val ColorTablero   = Color(0xFF0F3460)
+val ColorStats     = Color(0xFF16213E)
+val ColorVictoria  = Color(0xFFFFD700)
+val ColorBoton     = Color(0xFF533483)
